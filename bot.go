67afc93528974756fb269ced62503cab9ed34700
elianiva <dicha.arkana03@gmail.com>
2024-04-17 08:35:18 +0700

    feat: all commands are handled by gemini

--- bot.go
+++ bot.go
@@ -26,8 +26,7 @@ type Bot struct {
 }
 
 func (b *Bot) RegisterHandlers() {
-	b.client.AddEventHandler(b.pingHandler)
-	b.client.AddEventHandler(b.geminiHandler)
+	b.client.AddEventHandler(b.messageHandler)
 }
 
 func (b *Bot) Start() error {
@@ -64,82 +63,81 @@ func (b *Bot) Start() error {
 	return nil
 }
 
-func (b *Bot) pingHandler(evt interface{}) {
+// pingHandler is a simple handler that responds to a ping with a pong along with the response time
+func (b *Bot) handlePingEvent(evt *events.Message) {
 	startTime := time.Now()
-	switch v := evt.(type) {
-	case *events.Message:
-		msg := extractMessage(v)
-
-		if msg == "ping" {
-			fmt.Println("Sending pong")
-			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-			defer cancel()
-			v.Info.Sender.Device = 0
-			message, err := b.client.SendMessage(ctx, v.Info.Sender, &waProto.Message{
-				ExtendedTextMessage: &waProto.ExtendedTextMessage{
-					Text: proto.String("Pong! Response Time: " + strconv.FormatInt(time.Since(startTime).Nanoseconds(), 10) + "ns"),
-				},
-			})
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to send message")
-			} else {
-				log.Debug().Msgf("Sent message %+v", message)
-			}
-		}
+	fmt.Println("Sending pong")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	evt.Info.Sender.Device = 0
+	message, err := b.client.SendMessage(ctx, evt.Info.Sender, &waProto.Message{
+		ExtendedTextMessage: &waProto.ExtendedTextMessage{
+			Text: proto.String("Pong! Response Time: " + strconv.FormatInt(time.Since(startTime).Nanoseconds(), 10) + "ns"),
+		},
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to send message")
+	} else {
+		log.Debug().Msgf("Sent message %+v", message)
 	}
 }
 
-func (b *Bot) geminiHandler(env interface{}) {
-	switch v := env.(type) {
+func (b *Bot) handleGeminiEvent(msg string, evt *events.Message) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	evt.Info.Sender.Device = 0
+
+	// get previous chat contexts from cache
+	chatContext, err := getChatContext(b.cache, evt.Info.Sender)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get chat context")
+	}
+
+	// fetch answer from gemini
+	geminiAnswer, err := fetchGeminiResponse(strings.TrimPrefix(msg, "AI, "), chatContext.Items)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to fetch response from gemini")
+		geminiAnswer = "Maaf, saya tidak bisa membantu Anda saat ini."
+	}
+	if geminiAnswer == "" {
+		geminiAnswer = "Maaf, saya tidak bisa membantu Anda saat ini."
+	}
+
+	message, err := b.client.SendMessage(ctx, evt.Info.Sender, &waProto.Message{
+		ExtendedTextMessage: &waProto.ExtendedTextMessage{
+			Text: proto.String(geminiAnswer),
+		},
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to send message")
+	} else {
+		log.Debug().Msgf("Sent message %+v", message)
+	}
+
+	// store to chat context unique to each user
+	err = storeChatContext(b.cache, ChatContext{
+		SenderId: evt.Info.Sender,
+		Items: []Content{
+			{Role: "user", Parts: []Part{{Text: msg}}},
+			{Role: "model", Parts: []Part{{Text: geminiAnswer}}},
+		},
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to store chat context")
+	}
+}
+
+func (b *Bot) messageHandler(evt interface{}) {
+	switch v := evt.(type) {
 	case *events.Message:
 		msg := extractMessage(v)
-
-		fmt.Println("Received message: ", msg)
-
-		if strings.HasPrefix(msg, "AI, ") {
-			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-			defer cancel()
-			v.Info.Sender.Device = 0
-
-			// get previous chat contexts from cache
-			chatContext, err := getChatContext(b.cache, v.Info.Sender)
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to get chat context")
-			}
-
-			// fetch answer from gemini
-			geminiAnswer, err := fetchGeminiResponse(strings.TrimPrefix(msg, "AI, "), chatContext.Items)
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to fetch response from gemini")
-				geminiAnswer = "Maaf, saya tidak bisa membantu Anda saat ini."
-			}
-			if geminiAnswer == "" {
-				geminiAnswer = "Maaf, saya tidak bisa membantu Anda saat ini."
-			}
-
-			message, err := b.client.SendMessage(ctx, v.Info.Sender, &waProto.Message{
-				ExtendedTextMessage: &waProto.ExtendedTextMessage{
-					Text: proto.String(geminiAnswer),
-				},
-			})
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to send message")
-			} else {
-				log.Debug().Msgf("Sent message %+v", message)
-			}
-
-			// store to chat context unique to each user
-			err = storeChatContext(b.cache, ChatContext{
-				SenderId: v.Info.Sender,
-				Items: []Content{
-					{Role: "user", Parts: []Part{{Text: msg}}},
-					{Role: "model", Parts: []Part{{Text: geminiAnswer}}},
-				},
-			})
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to store chat context")
-			}
+		if msg == "ping" {
+			b.handlePingEvent(v)
+			return
 		}
+		// handle the rest of the messages using gemini
+		b.handleGeminiEvent(msg, v)
+		break
 	}
 }
 
